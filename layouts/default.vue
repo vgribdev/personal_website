a4d6a73911d1dd80d0b0a2ed4e20770bfb870b81
Crittografia <cristiano-raimondi@tiscali.it>
2020-03-04 17:15:06 +0100

    New layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <nuxt/>
+        <nuxt />
     </div>
 </template>
 <script lang="ts">
 </script>
 <style>
-    body {
-        font-family: 'Nunito Sans', sans-serif;
-    }
+body {
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 14px;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,51 @@
 import { Configuration } from '@nuxt/types'
 
 const config: Configuration = {
-  // Define your configuration with auto-completion & type checking
-     mode: 'universal',
+    // Define your configuration with auto-completion & type checking
+    mode: 'universal',
     /*
-    ** Headers of the page
-    */
+     ** Headers of the page
+     */
     head: {
         title: 'Valentin Grib',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Portfolio of Valentin Grib' }
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Portfolio of Valentin Grib'
+            }
         ],
         link: [
             { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap' }
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap'
+            }
         ]
     },
     /*
-    ** Customize the progress-bar color
-    */
+     ** Customize the progress-bar color
+     */
     loading: { color: '#fff' },
     /*
-    ** Global CSS
-    */
+     ** Global CSS
+     */
     css: ['static/reset.min.css'],
     /*
-    ** Plugins to load before mounting the App
-    */
+     ** Plugins to load before mounting the App
+     */
     plugins: [],
     /*
-    ** Nuxt.js modules
-    */
+     ** Nuxt.js modules
+     */
     modules: [],
 
-    buildModules: ['@nuxt/typescript-build'],
+    buildModules: ['@nuxt/typescript-build']
 }
 export default config
