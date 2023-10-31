653aa57ae98f98ffb59816aeeac744069b76c8a1
Valentin Grib <gribvalentin@outlook.com>
2023-10-31 12:06:11 +0100

    update title page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './global.css'
 import { GeistSans } from 'geist/font'
 
 export const metadata: Metadata = {
-    title: 'Create Next App',
+    title: 'Valentin Grib Portfolio',
     description:
         "Hi I'm Valentin Grib a web developer based in Italy. I like programming, math, socializing and sports."
 }
